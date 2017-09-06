82f0b3bd4d97a4cfd02ad45bcd6acac845aa13f3
Gleb Evgenov <glebevgenov@yandex.ru>
2017-09-06 13:02:11 +0300

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=test_siberianpro',
+    'username' => 'test_siberianpro',
+    'password' => 'free',
+    'charset' => 'utf8',
+];
